3c08917277c089e77373dac9696b689a1c34c361
ririyad <rasheduliriyad@gmail.com>
2021-11-20 16:25:08 +0600

    Initial commit, check in for day 0

---
+++ src/day-0/hello.rs
@@ -0,0 +1,30 @@
+fn main() {
+    println!("Hello from Rust");
+    println!("Hello from Rust again");
+    println!("{} days", 31);
+
+    // positional argument
+    println!("{0} this is {1}, {1} this is {0}", "Alice", "Bob");
+
+    //named argument
+    println!("{subject} {verb} {object}",
+            subject="the quick brown fox",
+            verb="jumps over",
+            object="the lazy dog");
+
+    //special formatting (converting to binary)
+    println!("{} is {:b} people know binary, the other one doesn't", 1, 2);
+
+    // special formatting (padding numbers)
+    println!("{number:>width$}", number=1, width=6);
+
+    // special formatting (padding numbers with extra 0)
+    println!("{number:0>width$}", number=1, width=6);
+
+    // activity
+    let pi = 3.141592;
+    // rounding pi value to 3 digits
+    println!("pi is {:.3}", pi);
+
+
+}
