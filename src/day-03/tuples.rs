7de8e2acc8d45a1de1bc161180e293bef7c7e5ba
ririyad <rasheduliriyad@gmail.com>
2021-11-23 22:53:46 +0600

    [day 3] playing with tuples
    Why am I using {:?}, for tuples?

---
+++ src/day-03/tuples.rs
@@ -0,0 +1,20 @@
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean, integer)
+}
+
+fn main() {
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                    -1i8, -2i16, -3i32, -4i64,
+                    0.1f32, 0.2f64,
+                    'a', true);
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.1);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8));
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    let pair = (1, true);
+    println!("pair is: {:?}", pair);
+    println!("The reversed pair is: {:?}", reverse(pair));
+}
